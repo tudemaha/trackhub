f9f275f2dcbe023799d5aa5816b290f4cf665470
tudemaha <gede_mahardika88@yahoo.co.id>
2022-10-14 11:59:08 +0800

    change name of db

--- utils/mysql_config.js
+++ utils/mysql_config.js
@@ -1,13 +1,12 @@
 const mysql = require('mysql2');
 require('dotenv').config({path: '../.env'});
-const fs = require('fs');
 
 const connection = mysql.createConnection({
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    database: process.env.DB_DASTABASE
+    database: process.env.DB_DATABASE
 });
 
 module.exports = connection;
