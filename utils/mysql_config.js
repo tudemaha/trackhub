7c4c1d9c89e60f9780137fef967f66ec62a4e665
tudemaha <gede_mahardika88@yahoo.co.id>
2022-10-14 11:47:50 +0800

    refactoring
    gitignore node_modules and config for mysql_config

--- utils/mysql_config.js
+++ utils/mysql_config.js
@@ -1,10 +1,18 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
+require('dotenv').config({path: '../.env'});
+const fs = require('fs');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'trackhub'
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_DASTABASE
 });
+
+connection.query('SHOW DATABASES', (error, result) => {
+    if(error) throw error;
+    console.log(result);
+})
 
 module.exports = connection;
